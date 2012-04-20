ff331429f38dbb022a011af72e08eaee2079a1fa
Mathy Vanhoef <vanhoefm@gmail.com>
2012-04-20 04:32:09 +0200

    Script actually updated now..

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from twisted.internet import reactor, defer
 from twisted.internet.endpoints import TCP4ServerEndpoint
 from twisted.protocols import portforward
 import re
+from socket import *
+import struct
 
+# getsockopt parameter
+SO_ORIGINAL_DST = 80
 
 mappings = dict()
 reversemappings = dict()
@@ -40,8 +44,23 @@ class ProxyResolver(client.Resolver):
 
 
 class ProxyClient(portforward.ProxyClient):
+    def __init__(self):
+        self.gotestablished = False
+        self.requestdata = None
+
+    def setRequestData(self, data):
+        self.requestdata = data
+
     def dataReceived(self, data):
-        portforward.ProxyClient.dataReceived(self, data)
+        if self.gotestablished or self.requestdata == None:
+            # Forward traffic to the client
+            portforward.ProxyClient.dataReceived(self, data)
+        else:
+            # TODO: Verify that received data is indeed "HTTP/1.0 200 Connection established"
+            self.gotestablished = True
+            # Send original HTTPS request
+            self.transport.write(self.requestdata)
+
 
 class ProxyClientFactory(portforward.ProxyClientFactory):
     protocol = ProxyClient
@@ -56,9 +75,24 @@ class ProxyServer(portforward.ProxyServer):
         self.headerre = re.compile(r'CONNECT (\d+\.\d+\.\d+\.\d+):\d+ HTTP')
         self.firstdata = None
 
+
     def dataReceived(self, data):
         if not self.receivedfirst:
-            print socket.getsockopt(self.transport.fileno(), SOL_IP, SO_ORIGINAL_DST, 16)
+
+            dst = socket.getsockopt(self.transport.socket, SOL_IP, SO_ORIGINAL_DST, 16)
+            srv_port, srv_ip = struct.unpack("!2xH4s8x", dst)
+
+            #TODO: Find better way to detect redirected traffic. Now we assume only HTTPS needs conversion.
+            if srv_port == 443:
+                # We assume redirect traffic is not yet proxied, so add CONNECT command
+                self.peer.setRequestData(data)
+                data = "CONNECT " + inet_ntoa(srv_ip) + ":" + str(srv_port) + " HTTP/1.1\r\n\r\n"
+            # NOTE: If you uncomment this elif block, your proxy must support invisible proxying
+            elif srv_port == 80:
+                # Rewrite to absolute GET request if info available
+		if reversemappings.has_key(inet_ntoa(srv_ip)):
+                    data = re.sub(r'^GET ', "GET http://" + reversemappings[inet_ntoa(srv_ip)] + ":" + str(srv_port), data)
+            
             result = self.headerre.match(data)
 
             if (result != None and reversemappings.has_key(result.group(1))):
@@ -77,13 +111,24 @@ class ProxyFactory(portforward.ProxyFactory):
     protocol = ProxyServer
     
     def doStart(self):
-        print "==== Android Proxy Up and Running ===="
+        print "\t==== Android Proxy Up and Running ====\n"
 
 
 def main():
+    print "AndroidProxy  ---   (C) Mathy Vanhoef (Made While Intern @ Ernst & Young)"
+    print "This program comes with ABSOLUTELY NO WARRANTY."
+    print
     print "DNS server will listen on localhost:65"
     print "HTTP Proxy will listen on localhost:8007"
+    print
+    print "Physical device: Configure your computer as router and dns server and execute"
+    print "\tiptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8007"
+    print "\tiptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 8007"
+    print
     print "Start emulator using command: emulator @AvdName -http-proxy http://localhost:8007 -dns-server localhost"
+    print
+    print "Don't forget to start your normal proxy on localhost:8080"
+    print
     
     # Setup custom DNS server
     resolvers = []
